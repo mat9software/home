3e1593af28288d9860bd9d764254a58fec213010
Mat9 Software <mat9software@gmail.com>
2023-01-14 23:04:55 -0400

    Copy existing Emscripten Imgui start setup

---
+++ stocks/dockspace.h
@@ -0,0 +1,7 @@
+#ifndef DOCKSPACE_H
+#define DOCKSPACE_H
+
+void ShowDockSpace();
+
+#endif //DOCKSPACE_H
+
---
+++ stocks/stocks.h
@@ -0,0 +1,6 @@
+#ifndef STOCKS_H
+#define STOCKS_H
+
+void show_stocks();
+
+#endif // STOCKS_H
